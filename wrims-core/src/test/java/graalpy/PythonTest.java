843cbb2a34c7d88412c24178732f1419ca9c2554
zack-rma <zack@rmanet.com>
2025-07-15 15:50:18 -0700

    Added Java interop test, updated HelloWorld test

--- wrims-core/src/test/java/graalpy/PythonTest.java
+++ wrims-core/src/test/java/graalpy/PythonTest.java
@@ -1,6 +1,7 @@
 package graalpy;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.nio.file.Path;
 import org.graalvm.polyglot.Context;
@@ -18,11 +19,34 @@ final class PythonTest {
             context.eval("python", "import sys");
             context.eval("python", "sys.path.append(sys_path)");
 
-            HelloWorld world = context.eval("python", "import HelloWorld; HelloWorld").as(HelloWorld.class);
+            HelloWorld world = context.eval("python", "from HelloWorld import HelloWorld; HelloWorld()").as(HelloWorld.class);
             world.main();
 
             String result = world.getText();
-            assertEquals("Hello, World 2!", result);
+            assertEquals("Hello, World!", result);
+        }
+    }
+
+    @Test
+    void testRunJavaTest() {
+        String path = "src/test/resources/python";
+        try (var context = createPythonContext(path)) {
+            // Configure the Python context to include the path to the Python resources, otherwise a ModuleNotFoundError will occur
+            context.getBindings("python").putMember("sys_path", path);
+            context.eval("python", "import sys");
+            context.eval("python", "sys.path.append(sys_path)");
+
+            JavaTest test = context.eval("python", "from JavaTest import JavaTest; JavaTest()").as(JavaTest.class);
+            test.main();
+
+            boolean isTrue = test.isTrue();
+            assertTrue(isTrue);
+
+            int number = test.getNumber(5, 10);
+            assertEquals(5, number);
+
+            String result = test.getText();
+            assertEquals("JavaTest instance with custom class", result);
         }
     }
 
