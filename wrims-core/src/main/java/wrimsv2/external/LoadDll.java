a9987787c4534c5309424e6a1c62c458127cdbb8
Zachary Roy <zachary.roy@water.ca.gov>
2025-10-20 13:48:56 -0700

    Add `LoadDll` constructor for `Path` argument

--- wrims-core/src/main/java/wrimsv2/external/LoadDll.java
+++ wrims-core/src/main/java/wrimsv2/external/LoadDll.java
@@ -1,8 +1,12 @@
 package wrimsv2.external;
 
+import java.nio.file.Path;
+
 public class LoadDll {
-	public LoadDll(String dllName){
-		//System.load(System.getenv("WRIMS_v2_path")+dllName);
+	public LoadDll(String dllName) {
 		System.loadLibrary(dllName.replace(".dll", ""));
+	}
+	public LoadDll(Path dllLocation) {
+		System.load(dllLocation.toAbsolutePath().toString());
 	}
 }
