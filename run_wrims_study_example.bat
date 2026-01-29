00ce5b50d7572628cdeafce62ceb62468a7f9892
Li <Bing.Li@water.ca.gov>
2026-01-28 20:43:34 -0800

    Use dual modes and fluent api for XA solver, adjust the log4j2.xml and run_wrims_study_example.bat file

--- run_wrims_study_example.bat
+++ run_wrims_study_example.bat
@@ -1,5 +1,6 @@
 @echo off
 REM PROJECT SETTINGS: PROJECT_DIR, and CONFIG_FILE variables as needed.
+
 set PROJECT_DIR=J:\wrims\projects\dcr2023
 set CONFIG_FILE=study.config
 
@@ -7,6 +8,15 @@ REM Set the JAVA_HOME environment variable to the path of your java 21 JDK
 set JAVA_HOME="J:\java\jdk\jdk_21.0.8_temurin"
 
 set temp_wrims2=".\foo"
+
+REM ============================
+REM XA Solver dual-mode toggle
+REM Default: legacy (no behavior change)
+REM To enable tolerant:
+REM   set WRIMS_XA_MODE=tolerant
+REM ============================
+if "%WRIMS_XA_MODE%"=="" set "WRIMS_XA_MODE=tolerant"
+echo [XA] WRIMS_XA_MODE=%WRIMS_XA_MODE%
 
 REM Add the required DLLs to the PATH. Do not change if this if run from the wrims-engine root directory.
 set PATH=%PATH%;wrims-core\build\tmp\x64
@@ -19,5 +29,15 @@ set MAIN_CLASS=gov.ca.water.wrims.engine.core.components.ControllerBatch
 set WRIMS_CORE_JAR="wrims-core\build\libs\*"
 set WRIMS_CORE_DEPENDENCIES="wrims-core\build\tmp\libs\*"
 
-%JAVA_HOME%\bin\java -Xmx4096m -Xss1024K -Dproject.dir="%PROJECT_DIR%" -cp "%WRIMS_CORE_JAR%;%WRIMS_CORE_DEPENDENCIES%" %MAIN_CLASS% -config=%PROJECT_DIR%\%CONFIG_FILE%
+
+REM NOTE: Keep original java args; only add -Dwrims.xa.mode=%WRIMS_XA_MODE%
+"%JAVA_HOME%\bin\java" ^
+  -Xmx4096m -Xss1024K ^
+  -Dproject.dir="%PROJECT_DIR%" ^
+  -Dwrims.xa.mode=%WRIMS_XA_MODE% ^
+  -cp "%WRIMS_CORE_JAR%;%WRIMS_CORE_DEPENDENCIES%" ^
+  %MAIN_CLASS% ^
+  -config=%PROJECT_DIR%\%CONFIG_FILE% ^
+  -Dlog4j.configurationFile="%LOG4J_CONFIG%"
+
 pause
