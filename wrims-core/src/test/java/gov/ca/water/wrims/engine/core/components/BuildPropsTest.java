5b4441240e7f788aeab60ff6ea0fce9389a0ec69
Zachary Roy <zachary.roy@water.ca.gov>
2026-03-06 13:17:27 -0800

    Test `BuildProps` file load failure

--- wrims-core/src/test/java/gov/ca/water/wrims/engine/core/components/BuildPropsTest.java
+++ wrims-core/src/test/java/gov/ca/water/wrims/engine/core/components/BuildPropsTest.java
@@ -9,4 +9,14 @@ class BuildPropsTest {
         BuildProps buildProps = new BuildProps();
         Assert.assertNotNull(buildProps.getVN(), "the version number should not be null");
     }
+
+    @Test
+    void testUnknownWhenNotFound() {
+        BuildProps buildProps = new BuildProps("not-a-real-file.properties");
+        Assert.assertEquals(
+                buildProps.getVN(),
+                BuildProps.UNKNOWN_VERSION_TAG,
+                "the version number should default to an unknown"
+        );
+    }
 }
